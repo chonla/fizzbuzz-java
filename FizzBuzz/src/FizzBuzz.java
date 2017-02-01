6053e658769509f3aea499995b71a230c099f5ec
Chonlasith Jucksriporn <chonla@capabilify.com>
2017-02-01 17:30:12 +0700

    Refactor: Use .ToString() instead of hardcoded text

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -2,13 +2,10 @@
  * Created by chonla on 2/1/2017 AD.
  */
 public class FizzBuzz {
-    public String get(int i) {
-        if (i == 1) {
-            return "1";
-        }
+    public String get(Integer i) {
         if (i == 3) {
             return "Fizz";
         }
-        return "2";
+        return i.toString();
     }
 }
